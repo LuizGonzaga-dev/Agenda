50752233fcc21937eb8a888b67bcb030d31ceb27
LuizGonzaga <75226617+LuizGonzaga-dev@users.noreply.github.com>
2024-07-24 18:49:03 -0300

    Add project files.

---
+++ Agenda/Data/AgendaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Data
+{
+    public class AgendaDbContext : DbContext
+    {
+        public AgendaDbContext(DbContextOptions<AgendaDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Agenda/Program.cs
@@ -0,0 +1,37 @@
+using Agenda.Data;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//configuração do banco
+var connectionString = builder.Configuration.GetConnectionString("ConnectionMysql");
+
+builder.Services.AddDbContext<AgendaDbContext>(opt => opt.UseMySql(
+    connectionString,
+    ServerVersion.Parse("8.2.0")
+ ));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
